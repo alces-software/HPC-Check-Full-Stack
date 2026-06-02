fe80b2541e675883c8a29f0d038e3ad054306fab
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-06-02 11:14:53 +0100

    Update controller.js

--- backend/endpoints/rota/controller.js
+++ backend/endpoints/rota/controller.js
@@ -10,10 +10,35 @@ module.exports = (db) => {
     */
    async function getRota(req, res) {
       try {
-         const collection = await db.collection('schedule');
-         const results = await collection.find({})
-            .toArray();
-         console.log(results);
+         const days = await db.collection('schedule').find({}).toArray();
+         const people = await db.collection('person').find({}).toArray();
+         const clusters = await db.collection('cluster').find({}).toArray();
+
+         let response = {
+            mon: {},
+            tue: {},
+            wed: {},
+            thu: {},
+            fri: {}
+         }
+
+         const dayNames = Object.keys(response);
+
+         days.forEach((day) => {
+            const dayName = dayNames[day.dayIndex];
+            const personName = people.find(data => data._id.toString() == day.personId).name;
+            const clusterName = clusters.find(data => data._id.toString() == day.clusterId).name;
+
+            if (Object.hasOwn(response[dayName], personName)) {
+               response[dayName][personName].push(clusterName)
+            } else {
+               response[dayName] = {
+                  [personName]: [clusterName]
+               }
+            }
+         });
+
+         res.status(200).json({ success: true, body: response });
       } catch (error) {
          res.status(500).json({ success: false, error: error.message });
       }
