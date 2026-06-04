c236834a9d9293020af21bec0748720a313e1abc
Calum Murphy <calummurphy@Calums-MacBook-Air.local>
2026-06-04 16:17:08 +0100

    Adding Alces favicon image

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header"
+import type { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,12 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/images/alces_logo.png",
+  },
+};
 
 export default function RootLayout({
   children,
