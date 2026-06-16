9a05bfa319948a8a1cb2f2038b7a0aa40d00d2a6
Oscar Thomson <oscmog@gmail.com>
2026-06-16 10:04:22 +0000

    Updated scheduler to generate schedules for all teams

--- backend/services/cron/methods/schedule.js
+++ backend/services/cron/methods/schedule.js
@@ -2,57 +2,67 @@
 module.exports.generateSchedule = async (db) => {
     console.log("Generating schedule");
 
-    // Get all people from the database
-    let peopleIDs = await db.collection('person').find({}).project({ _id: 1 }).toArray().then(results => results.map((id) => id._id.toString()));
-    // Get all clusters from the database
-    let clusterIDs = await db.collection('cluster').find({}).project({ _id: 1 }).toArray().then(results => results.map((id) => id._id.toString()));
+    await db.collection('schedule').deleteMany({})
+    // Get each team
+    let teamIds = await db.collection('team').find({}).project({ _id: 1 }).toArray().then(results => results.map((id) => id._id.toString()));
 
-    const per_person = Number(process.env.CLUSTERS_PER_PERSON) || 1
-    const per_day = Number(process.env.PEOPLE_PER_DAY) || 1
+    // Generate a schedule for each team
 
-    const shuffle = (array) => {
-        const shuffled = [...array]
-        for (let i = shuffled.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
-        }
-        return shuffled
-    }
+    for (let i=0; i<teamIds.length; i++){
+        console.log(`Creating schecdule for team ${i}`)
+        // Get all people from the database
+        let peopleIDs = await db.collection('person').find({ teamId: teamIds[i] }).project({ _id: 1  }).toArray().then(results => results.map((id) => id._id.toString()));
+        console.log(peopleIDs)
+        // Get all clusters from the database
+        let clusterIDs = await db.collection('cluster').find({ teamId: teamIds[i] }).project({ _id: 1 }).toArray().then(results => results.map((id) => id._id.toString()));
+        console.log(clusterIDs)
 
-    const scheduleEntries = []
-    const peoplePool = shuffle(peopleIDs)
-    const clusterPool = shuffle(clusterIDs)
+        const per_person = Number(process.env.CLUSTERS_PER_PERSON) || 1
+        const per_day = Number(process.env.PEOPLE_PER_DAY) || 1
 
-    const getNextFromPool = (pool, source) => {
-        if (pool.length === 0) {
-            pool.push(...shuffle(source))
-        }
-        return pool.pop()
-    }
-
-    for (let day = 0; day < 5; day++) {
-        const selectedPeople = []
-
-        for (let j = 0; j < per_day; j++) {
-            const personId = getNextFromPool(peoplePool, peopleIDs)
-            selectedPeople.push(personId)
+        const shuffle = (array) => {
+            const shuffled = [...array]
+            for (let i = shuffled.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+            }
+            return shuffled
         }
 
-        for (const personId of selectedPeople) {
-            for (let j = 0; j < per_person; j++) {
-                const clusterId = getNextFromPool(clusterPool, clusterIDs)
-                scheduleEntries.push({
-                    clusterId: clusterId,
-                    personId: personId,
-                    dayIndex: day
-                })
+        const scheduleEntries = []
+        const peoplePool = shuffle(peopleIDs)
+        const clusterPool = shuffle(clusterIDs)
+
+        const getNextFromPool = (pool, source) => {
+            if (pool.length === 0) {
+                pool.push(...shuffle(source))
+            }
+            return pool.pop()
+        }
+
+        for (let day = 0; day < 5; day++) {
+            const selectedPeople = []
+
+            for (let j = 0; j < per_day; j++) {
+                const personId = getNextFromPool(peoplePool, peopleIDs)
+                selectedPeople.push(personId)
+            }
+
+            for (const personId of selectedPeople) {
+                for (let j = 0; j < per_person; j++) {
+                    const clusterId = getNextFromPool(clusterPool, clusterIDs)
+                    scheduleEntries.push({
+                        clusterId: clusterId,
+                        personId: personId,
+                        dayIndex: day
+                    })
+                }
             }
         }
-    }
 
-    if (scheduleEntries.length > 0) {
-        await db.collection('schedule').deleteMany({})
-        await db.collection('schedule').insertMany(scheduleEntries)
+        if (scheduleEntries.length > 0) {
+            await db.collection('schedule').insertMany(scheduleEntries)
+        }
     }
 }
 
