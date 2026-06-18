ddc64776a2970e33857b5e7f8f324a7e76daf1c6
Oscar Thomson <oscmog@gmail.com>
2026-06-18 12:39:43 +0000

    Edited weekly endpoint to match outputs of other endpoints

--- backend/endpoints/rota/controller/getRotaByWeek.js
+++ backend/endpoints/rota/controller/getRotaByWeek.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const {getWeekly} = require('../scheduleLogic');
+const { ObjectId } = require('mongodb');
 
 /**
  * @param {import('mongodb').Db} db
@@ -22,10 +23,74 @@ module.exports = (db) => {
             });
          } 
 
-         const response = await getWeekly(db, dateObj);
-         return res.status(200).json({ success: true, body: response });
+        const weekly = await getWeekly(db);
+
+         const personIds = new Set();
+         const clusterIds = new Set();
+
+         for (const assignments of Object.values(weekly)) {
+            for (const [personId, cIds] of Object.entries(assignments)) {
+               personIds.add(personId);
+
+               if (Array.isArray(cIds)) {
+                  cIds.forEach(id => clusterIds.add(id));
+               }
+            }
+         }
+
+         const personObjectIds = [...personIds].map(id => new ObjectId(id));
+         const clusterObjectIds = [...clusterIds].map(id => new ObjectId(id));
+
+         const [people, clusters] = await Promise.all([
+            personObjectIds.length
+               ? db.collection("person").find({ _id: { $in: personObjectIds } }).toArray()
+               : [],
+            clusterObjectIds.length
+               ? db.collection("cluster").find({ _id: { $in: clusterObjectIds } }).toArray()
+               : []
+         ]);
+
+         const peopleMap = new Map(
+         people.map(p => [p._id.toString(), p.name])
+         );
+
+         const clusterMap = new Map(
+         clusters.map(c => [c._id.toString(), c.name])
+         );
+
+         const enriched = Object.fromEntries(
+         Object.entries(weekly).map(([day, assignments]) => [
+            day,
+            Object.fromEntries(
+               Object.entries(assignments).map(([personId, cIds]) => {
+               const personName = peopleMap.get(personId) ?? personId;
+
+               return [
+                  personName,
+                  {
+                     id: personId,
+                     clusters: (cIds || []).map(id => ({
+                     id,
+                     name: clusterMap.get(id) ?? id
+                     }))
+                  }
+               ];
+               })
+            )
+         ])
+         );
+         return res.status(200).json({
+         success: true,
+         body: enriched
+         });
+
       } catch (error) {
-         return res.status(500).json({ success: false, error: error.message });
+         console.error("rota enriched error:", error);
+
+         return res.status(500).json({
+         success: false,
+         error: error.message
+         });
       }
    };
 };
