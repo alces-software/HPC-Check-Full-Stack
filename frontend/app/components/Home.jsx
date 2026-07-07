692a97cd29b6eccac9956886eefe638319e2fa24
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-07-07 14:07:12 +0100

    Update Home.jsx

--- frontend/app/components/Home.jsx
+++ frontend/app/components/Home.jsx
@@ -11,19 +11,46 @@ function PortalIcon() {
       <svg viewBox="0 0 80 80" className="h-20 w-20">
          <defs>
             <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
-               <feDropShadow
-                  dx="4"
-                  dy="4"
-                  stdDeviation="2"
-                  floodOpacity="0.25"
-               />
+               <feDropShadow dx="4" dy="4" stdDeviation="2" floodOpacity="0.25" />
             </filter>
          </defs>
 
-         <rect x="10" y="10" width="30" height="30" rx="5" className="fill-green-300" filter="url(#shadow)" />
-         <rect x="44" y="10" width="30" height="30" rx="5" className="fill-blue-300 " filter="url(#shadow)" />
-         <rect x="10" y="44" width="30" height="30" rx="5" className="fill-purple-300" filter="url(#shadow)" />
-         <rect x="44" y="44" width="30" height="30" rx="5" className="fill-amber-300" filter="url(#shadow)" />
+         <rect
+            x="10"
+            y="10"
+            width="30"
+            height="30"
+            rx="5"
+            className="fill-green-300"
+            filter="url(#shadow)"
+         />
+         <rect
+            x="44"
+            y="10"
+            width="30"
+            height="30"
+            rx="5"
+            className="fill-blue-300 "
+            filter="url(#shadow)"
+         />
+         <rect
+            x="10"
+            y="44"
+            width="30"
+            height="30"
+            rx="5"
+            className="fill-purple-300"
+            filter="url(#shadow)"
+         />
+         <rect
+            x="44"
+            y="44"
+            width="30"
+            height="30"
+            rx="5"
+            className="fill-amber-300"
+            filter="url(#shadow)"
+         />
       </svg>
    );
 }
