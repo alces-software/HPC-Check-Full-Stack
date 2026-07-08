e07eccae2f7e983d42daed3211b7474ded1d31a4
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-07-08 09:02:20 +0100

    test

--- backend/endpoints/hpc/routes.js
+++ backend/endpoints/hpc/routes.js
@@ -8,7 +8,7 @@ module.exports = (db) =>
       .get('/hpc', require('./controller/getAllHpc')(db))
       .get('/hpc/id/:id', require('./controller/getHpcById')(db))
       .get('/hpc/name/:name', require('./controller/getHpcByName')(db))
-      .get('/hpc/pool/not/:id', require('./controller/getHpcNotInPool')(db))
+      .get('/hpc/pool/not/:id',  require('./controller/getHpcNotInPool')(db))
       .get('/hpc/pool/:id', require('./controller/getHpcByPool')(db))
       // POST
       .post('/hpc', require('./controller/addHpc')(db))
@@ -16,4 +16,4 @@ module.exports = (db) =>
       .delete('/hpc/:id', require('./controller/deleteHpc')(db))
       // UPDATE
       .patch('/hpc/pool/add/:id', require('./controller/addHpcToPool')(db))
-      .patch('/hpc/pool/remove/:id', require('./controller/removeHpcFromPool')(db));
+      .patch('/hpc/pool/remove/:id', require('./controller/removeHpcFromPool')(db))
